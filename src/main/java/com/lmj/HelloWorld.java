24d00d1e919e649bb87f050d6d2ce3a5104f8e45
WoXinWuYou <40634434+WoXinWuYou@users.noreply.github.com>
2020-06-13 10:59:47 +0800

    git维护HelloWorld代码

--- src/main/java/com/lmj/HelloWorld.java
+++ src/main/java/com/lmj/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("eclipse添加！");
 		System.out.println("git添加！");
+                System.out.println("git添加-2020-6-13 10:59:15！");
 	}
 }
