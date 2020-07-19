4b90e0c5a711808ce4d4c3d207030e9d952b6979
del-lmj <2057113352@qq.com>
2020-07-19 11:35:13 +0800

    清空内容

--- src/main/java/com/lmj/HelloWorld.java
+++ src/main/java/com/lmj/HelloWorld.java
@@ -2,20 +2,6 @@ package com.lmj;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("eclipse添加！");
 		System.out.println("git添加！");
-		/*git添加*/
-		System.out.println("git添加-2020-6-13 10:59:15！");
-		System.out.println("git添加-2020-6-13 11:11:53！");
-		System.out.println("git添加-2020-6-13 11:11:53！");
-		System.out.println("eclipse添加-2020-6-13 11:11:53！");
-		System.out.println("eclipse添加-2020-6-13 11:22:13！");
-		System.out.println("eclipse添加-2020-6-13 11:26:06！");
-		System.out.println("git-2020-6-13 11:31:29！");
-		System.out.println("eclipse-2020-6-13 11:43:22！");
-		System.out.println("git-2020-6-14 10:46:09！");
-		System.out.println("eclipse-2020-6-14 10:45:23！");
-		System.out.println("eclipse-2020-6-14 14:29:10！");
-		System.out.println("git-2020-6-14 14:29:40！");
 	}
 }
--- src/main/java/com/lmj/HelloWorldEclipse.java
+++ src/main/java/com/lmj/HelloWorldEclipse.java
@@ -3,16 +3,5 @@ package com.lmj;
 public class HelloWorldEclipse {
 	public static void main(String[] args) {
 		System.out.println("eclipse添加！");
-		System.out.println("git添加！");
-		System.out.println("eclipse添加-2020-6-13 10:58:07！");
-		System.out.println("eclipse添加-2020-6-13 11:12:38！");
-		System.out.println("eclipse添加-2020-6-13 11:14:58！");
-		System.out.println("eclipse添加-2020-6-13 11:21:26！");
-		System.out.println("eclipse添加-2020-6-13 11:25:47！");
-		System.out.println("eclipse添加-2020-6-13 11:30:46！");
-		System.out.println("git添加-2020-6-13 11:32:32！");
-		System.out.println("eclise添加-2020-6-13 11:43:35！");
-		System.out.println("git添加-2020-6-14 10:45:29！");
-		System.out.println("eclipse添加-2020-6-14 14:29:01！");
 	}
 }
