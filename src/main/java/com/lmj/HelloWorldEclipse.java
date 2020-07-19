a69d9a5b0248c22097dd6e18297dd4586d8bfc3a
WoXinWuYou <40634434+WoXinWuYou@users.noreply.github.com>
2020-07-19 11:36:13 +0800

    browser1

--- src/main/java/com/lmj/HelloWorldEclipse.java
+++ src/main/java/com/lmj/HelloWorldEclipse.java
@@ -3,5 +3,6 @@ package com.lmj;
 public class HelloWorldEclipse {
 	public static void main(String[] args) {
 		System.out.println("eclipse添加！");
+		System.out.println("eclipse添加！browser1");		
 	}
 }
