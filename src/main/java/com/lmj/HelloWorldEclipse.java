dac308bf650b8442769408037b293ebd15b50ee7
WoXinWuYou <40634434+WoXinWuYou@users.noreply.github.com>
2021-04-30 09:21:53 +0800

    Create HelloWorldEclipse.java
    修改2021-4-30 09:21:37

--- src/main/java/com/lmj/HelloWorldEclipse.java
+++ src/main/java/com/lmj/HelloWorldEclipse.java
@@ -3,7 +3,5 @@ package com.lmj;
 public class HelloWorldEclipse {
 	public static void main(String[] args) {
 		System.out.println("eclipse添加！");
-		System.out.println("eclipse添加！browser1");	
-		System.out.println("eclipse添加！eclipse1");
 	}
 }
